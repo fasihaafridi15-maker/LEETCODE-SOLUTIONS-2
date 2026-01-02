b947f53ce2cdd083bfebaa15c88f6ab0c465c301
fasihaafridi15-maker <fasihaafridi15@gmail.com>
2026-01-02 14:07:37 -0800

    Time: 4 ms (99.71%) | Memory: 47 MB (45.42%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        // Push into minStack if empty or val is <= current min
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int removed = stack.pop();
+
+        // If popped value equals minStack top, pop minStack also
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
